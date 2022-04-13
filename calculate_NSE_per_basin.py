c2664a9ef1c4d89579de1ee85b4c2a2357875753
Ran Galun <galun.ran@gmail.com>
2022-04-13 11:06:44 +0300

    adding CDF calculation to the script

--- calculate_NSE_per_basin.py
+++ calculate_NSE_per_basin.py
@@ -1,5 +1,6 @@
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 import re
+import numpy as np
 
 PATTERN = r"The NSE loss for basin with ID: (\d+) is: ([-+]?\d*[.,]?\d*)"
 
@@ -18,9 +19,26 @@ def create_dict_basin_id_to_NSE(logs_filename):
     return d
 
 
+def plot_CDF_NSE_basins(dict_basins_mean_NSE_loss):
+    nse_losses = []
+    for basin_id, mean_nes_loss in dict_basins_mean_NSE_loss.items():
+        if mean_nes_loss >= 0:
+            nse_losses.append(mean_nes_loss)
+    nse_losses_np = np.array(nse_losses)
+    # taken from https://stackoverflow.com/questions/15408371/cumulative-distribution-plots-python
+    # evaluate the histogram
+    values, base = np.histogram(nse_losses_np, bins=1000)
+    # evaluate the cumulative
+    cumulative = np.cumsum(values)
+    # plot the cumulative function
+    plt.plot(base[:-1], cumulative, c='blue')
+    plt.show()
+
+
 def main():
     with open("./nse_per_basin", "w") as f:
         d = create_dict_basin_id_to_NSE("./output.log")
+        dict_basins_id_to_mean_nse_loss = {}
         for basin_id, nse_losses_list in d.items():
             print("Basin ID: {}, NSE losses: {}, mean NSE losses: {}".format(basin_id, nse_losses_list,
                                                                              sum(nse_losses_list) / len(
@@ -28,6 +46,8 @@ def main():
             f.write("Basin ID: {}, NSE losses: {}, mean NSE losses: {}\n".format(basin_id, nse_losses_list,
                                                                                  sum(nse_losses_list) / len(
                                                                                      nse_losses_list)))
+            dict_basins_id_to_mean_nse_loss[basin_id] = sum(nse_losses_list) / len(nse_losses_list)
+    plot_CDF_NSE_basins(dict_basins_id_to_mean_nse_loss)
 
 
 if __name__ == "__main__":
