3b6841fd373f16656663181f97f138c1d33fdf7a
Ran <galun.ran@gmail.com>
2022-05-20 18:34:34 +0300

    adding the script of Dor Assignment 3 in C

--- src/Dor_Assignment_3_tests.py
+++ src/Dor_Assignment_3_tests.py
@@ -7,7 +7,7 @@ def main():
     num_equations = 3
     for i in range(1):
         input_str += str(num_equations) + "\r\n"
-        mat = np.random.rand(num_equations, num_equations).astype('f', order='C') * 100
+        mat = np.random.rand(num_equations, num_equations + 1).astype('f', order='C') * 10
         # num_spaces = np.zeros((num_equations, 1))
         num_spaces = np.random.randint(1, 2, (num_equations, 1))
         for j in range(mat.shape[0]):
@@ -18,6 +18,8 @@ def main():
                     input_str += str(mat[j, k]) + ' ' * int(num_spaces[j].item()) + ' * y + '
                 elif k == 2:
                     input_str += str(mat[j, k]) + ' ' * int(num_spaces[j].item()) + ' * z'
+                elif k == 3:
+                    input_str += ' ' * int(num_spaces[j].item()) + '=' + str(mat[j, k])
             input_str += "\r\n"
     print(input_str)
     p = subprocess.Popen(
@@ -29,6 +31,8 @@ def main():
         p.kill()
         outs, errs = p.communicate()
     print(outs.decode())
+    print(np.linalg.det(mat[:, :-1]))
+    print(np.linalg.solve(mat[:, :-1], mat[:, -1]))
 
 
 if __name__ == "__main__":
