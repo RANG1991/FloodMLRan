a53017017bbd9c1fbc798a2c679fb6a111e1a787
Ran <galun.ran@gmail.com>
2022-05-21 09:25:57 +0300

    adding the script of Dor Assignment 3 in C

--- src/Dor_Assignment_3_tests.py
+++ src/Dor_Assignment_3_tests.py
@@ -4,35 +4,38 @@ import numpy as np
 
 def main():
     input_str = ""
-    num_equations = 3
-    for i in range(1):
+    num_equations = 2
+    num_iter = 10
+    for i in range(num_iter):
         input_str += str(num_equations) + "\r\n"
         mat = np.random.rand(num_equations, num_equations + 1).astype('f', order='C') * 10
         # num_spaces = np.zeros((num_equations, 1))
         num_spaces = np.random.randint(1, 2, (num_equations, 1))
         for j in range(mat.shape[0]):
-            for k in range(mat.shape[1]):
+            for k in range(mat.shape[1] - 1):
                 if k == 0:
                     input_str += str(mat[j, k]) + ' ' * int(num_spaces[j].item()) + ' * x + '
                 elif k == 1:
                     input_str += str(mat[j, k]) + ' ' * int(num_spaces[j].item()) + ' * y + '
                 elif k == 2:
                     input_str += str(mat[j, k]) + ' ' * int(num_spaces[j].item()) + ' * z'
-                elif k == 3:
-                    input_str += ' ' * int(num_spaces[j].item()) + '=' + str(mat[j, k])
+            input_str += ' ' * int(num_spaces[j].item()) + '=' + str(mat[j, -1])
             input_str += "\r\n"
-    print(input_str)
+        if i < (num_iter - 1):
+            input_str += "y\r\n"
+        else:
+            input_str += "n\r\n"
     p = subprocess.Popen(
         [r"C:\Users\galun\Desktop\Ran\Dor_Assignments_C\Assignment_3_Dor\X64\Debug\Dor_Assignment_3.exe"],
         stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     try:
         outs, errs = p.communicate(input=input_str.encode("ascii"))
+        p.terminate()
     except subprocess.TimeoutExpired:
         p.kill()
         outs, errs = p.communicate()
+    print(p.returncode)
     print(outs.decode())
-    print(np.linalg.det(mat[:, :-1]))
-    print(np.linalg.solve(mat[:, :-1], mat[:, -1]))
 
 
 if __name__ == "__main__":
