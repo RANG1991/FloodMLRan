2320317452cf5f64a8d37d71a011fb0657e4330f
Ran Galun <ranga@cs.huji.ac.il>
2022-05-22 14:46:58 +0300

    deleting inner neuralhydrology dir as it's redandunt

--- neuralhydrology/neuralhydrology/utils/errors.py
+++
@@ -1,6 +0,0 @@
-class NoTrainDataError(Exception):
-    """Raised, when basin contains no valid discharge in training period"""
-
-
-class AllNaNError(Exception):
-    """Raised by `calculate_(all_)metrics` if all observations or all simulations are NaN. """
