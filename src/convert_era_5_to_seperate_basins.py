e90a997b2d087906c3691f35905cf725f04c9668
Ran <galun.ran@gmail.com>
2022-05-30 20:47:35 +0300

    trying to fix differences to Frederik's work

--- src/convert_era_5_to_seperate_basins.py
+++ src/convert_era_5_to_seperate_basins.py
@@ -126,7 +126,7 @@ def parse_single_basin_precipitation(station_id, basin_data, discharge_file_name
             ind_lat_max = np.squeeze(np.argwhere(lat == min(max_lat, max_lat_array)))
             started_reading_data = True
         # multiply by 1000 to convert from meter to mm
-        tp = np.asarray(dataset['tp'][:, ind_lat_max:ind_lat_min + 1, ind_lon_min:ind_lon_max + 1]) * 1000
+        tp = np.asarray(dataset['tp'][:, ind_lat_max:ind_lat_min + 1, ind_lon_min:ind_lon_max + 1])
         tp[tp < 0] = 0
         # convert the time to datetime format and append it to the times array
         times = [datetime.datetime.strptime("1900-01-01 00:00", "%Y-%m-%d %H:%M") + datetime.timedelta(hours=int(ti[i]))
@@ -139,7 +139,7 @@ def parse_single_basin_precipitation(station_id, basin_data, discharge_file_name
     datetimes = np.concatenate(list_of_dates, axis=0)
     # concatenate the precipitation data from all the years
     precip = np.concatenate(list_of_total_precipitations, axis=0)
-    percip_mean_lat_lon = np.mean(precip, axis=(1, 2))
+    percip_mean_lat_lon = np.sum(precip, axis=(1, 2))
     df_percip_times = pd.DataFrame(data=datetimes, index=datetimes)
     datetimes = df_percip_times.index.to_pydatetime()
     ls = [[percip_mean_lat_lon[i]] for i in range(0, len(datetimes))]
