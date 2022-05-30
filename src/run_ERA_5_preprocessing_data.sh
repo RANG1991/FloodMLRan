60c7e472ef91066ac9377247269aed7a5adefb95
Ran <galun.ran@gmail.com>
2022-05-30 08:58:21 +0300

    moving python and sh files to the src folder from the data folder

---
+++ src/run_ERA_5_preprocessing_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Change number of tasks, amount of memory and time limit according to your needs
+
+#SBATCH -n 1
+#SBATCH --time=50:0:0
+#SBATCH --mem=40G
+#SBATCH --gres gpu
+#SBATCH -J jupyter
+
+# Uncomment and enter path of code
+cd /sci/labs/efratmorin/ranga/FloodMLRan/data/ERA5/
+
+# virtual_env location
+virtual_env=/sci/labs/efratmorin/ranga/FloodsMLEnv/bin/activate
+
+source $virtual_env
+# module load cuda/11.2
+
+python convert_era_5_to_seperate_basins.py
