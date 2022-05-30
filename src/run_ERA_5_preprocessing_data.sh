9931eee539514d7a33e1cb107c0e23c8fdbe4d42
Ran <galun.ran@gmail.com>
2022-05-30 09:11:32 +0300

    changing the script of run_ERA5_preprocessing_data.sh - changing the paths

--- src/run_ERA_5_preprocessing_data.sh
+++ src/run_ERA_5_preprocessing_data.sh
@@ -9,7 +9,7 @@
 #SBATCH -J jupyter
 
 # Uncomment and enter path of code
-cd /sci/labs/efratmorin/ranga/FloodMLRan/data/ERA5/
+cd /sci/labs/efratmorin/ranga/FloodMLRan/
 
 # virtual_env location
 virtual_env=/sci/labs/efratmorin/ranga/FloodsMLEnv/bin/activate
@@ -17,4 +17,4 @@ virtual_env=/sci/labs/efratmorin/ranga/FloodsMLEnv/bin/activate
 source $virtual_env
 # module load cuda/11.2
 
-python convert_era_5_to_seperate_basins.py
+python ./src/convert_era_5_to_seperate_basins.py
