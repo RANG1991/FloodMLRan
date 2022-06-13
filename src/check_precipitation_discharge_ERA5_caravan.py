462b2f6025abce670f1a9b42d71984123fc40b77
Ran <galun.ran@gmail.com>
2022-06-13 11:52:02 +0300

    new script to compare between caravan and ERA5

--- src/check_precipitation_discharge_ERA5_caravan.py
+++ src/check_precipitation_discharge_ERA5_caravan.py
@@ -8,6 +8,7 @@ def read_data_from_ERA5_dataset():
     df_flow = pd.read_csv("C:\\Users\\galun\\Desktop\\dis24_01031500.csv")
     df_flow = df_flow[["date", "flow"]]
     df = df_precip.merge(df_flow, on="date")
+    df = df.rename(columns={"precip": "precip_ERA5", "flow": "flow_ERA5"})
     return df
 
 
@@ -15,15 +16,15 @@ def read_data_from_caravan_dataset():
     df = pd.read_csv("C:\\Users\\galun\\Desktop\\Caravan\\timeseries\\"
                      "csv\\us\\us_01031500.csv")
     df = df[["date", "total_precipitation_sum", "streamflow"]]
-    df = df.rename(columns={"total_precipitation_sum": "precip", "streamflow": "flow"})
+    df = df.rename(columns={"total_precipitation_sum": "precip_caravan", "streamflow": "flow_caravan"})
     return df
 
 
 def main():
     df_ERA5 = read_data_from_ERA5_dataset()
     df_caravan = read_data_from_caravan_dataset()
-    df_precip_merged = df_ERA5[["date", "precip"]].merge(df_caravan[["date", "precip"]], on="date")
-    df_flow_merged = df_ERA5[["date", "flow"]].merge(df_caravan[["date", "flow"]], on="date")
+    df_precip_merged = df_ERA5[["date", "precip_ERA5"]].merge(df_caravan[["date", "precip_caravan"]], on="date").iloc[:50]
+    df_flow_merged = df_ERA5[["date", "flow_ERA5"]].merge(df_caravan[["date", "flow_caravan"]], on="date").iloc[:50]
     df_precip_merged.plot(x="date")
     df_flow_merged.plot(x="date")
     plt.show()
