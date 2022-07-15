dec4204e5e450529dab7df1cd066084e80a87f0c
Ran <galun.ran@gmail.com>
2022-07-15 10:09:44 +0300

    changing lab_share to ranga directory in configuration and shell script files

--- src/run_floods_ml_frederik_code.sh
+++ src/run_floods_ml_frederik_code.sh
@@ -3,13 +3,13 @@
 # Change number of tasks, amount of memory and time limit according to your needs
 
 #SBATCH -n 1
-#SBATCH --time=3:0:0
+#SBATCH --time=10:0:0
 #SBATCH --mem=40G
 #SBATCH --gres gpu
 #SBATCH -J jupyter
 
 # Uncomment and enter path of code
-cd /sci/labs/efratmorin/lab_share/FloodsML/
+cd /sci/labs/efratmorin/ranga/FloodsML/
 
 # virtual_env location
 virtual_env=/sci/labs/efratmorin/lab_share/FloodsMLEnv/bin/activate
@@ -18,4 +18,4 @@ source $virtual_env
 # module load cuda/11.2
 
 # Start Running NeuralHydrology code of Frederik
-./neuralhydrology/neuralhydrology/nh_run_scheduler.py train --directory /sci/labs/efratmorin/lab_share/FloodsML/config_files_dir --runs-per-gpu 1 --gpu-ids 0
+./neuralhydrology/neuralhydrology/nh_run_scheduler.py train --directory /sci/labs/efratmorin/ranga/FloodsML/config_files_dir --runs-per-gpu 1 --gpu-ids 0
