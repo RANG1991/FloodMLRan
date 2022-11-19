e859834b19b2a953c9105bb80f5d0f35b0dff452
Ran <galun.ran@gmail.com>
2022-11-19 18:36:07 +0200

    continue to implementing transformer

--- src/ERA5_transformer.py
+++ src/ERA5_transformer.py
@@ -30,7 +30,28 @@ class ERA5_Transformer(nn.Module):
         )
         self.out = nn.Linear(dim_model, num_tokens)
 
-    def forward(self, src, tgt):
+    def get_tgt_mask(self, size) -> torch.tensor:
+        # Generates a square matrix where each row allows one word more to be seen
+        mask = torch.tril(torch.ones(size, size) == 1)  # Lower triangular matrix
+        mask = mask.float()
+        mask = mask.masked_fill(mask == 0, float('-inf'))  # Convert zeros to -inf
+        mask = mask.masked_fill(mask == 1, float(0.0))  # Convert ones to 0
+
+        # EX for size=5:
+        # [[0., -inf, -inf, -inf, -inf],
+        #  [0.,   0., -inf, -inf, -inf],
+        #  [0.,   0.,   0., -inf, -inf],
+        #  [0.,   0.,   0.,   0., -inf],
+        #  [0.,   0.,   0.,   0.,   0.]]
+
+        return mask
+
+    def create_pad_mask(self, matrix: torch.tensor, pad_token: int) -> torch.tensor:
+        # If matrix = [1,2,3,0,0,0] where pad_token=0, the result mask is
+        # [False, False, False, True, True, True]
+        return matrix == pad_token
+
+    def forward(self, src, tgt, tgt_mask=None, src_pad_mask=None, tgt_pad_mask=None):
         # Src size must be (batch_size, src sequence length)
         # Tgt size must be (batch_size, tgt sequence length)
 
@@ -45,7 +66,8 @@ class ERA5_Transformer(nn.Module):
         tgt = tgt.permute(1, 0, 2)
 
         # Transformer blocks - Out size = (sequence length, batch_size, num_tokens)
-        transformer_out = self.transformer(src, tgt)
+        transformer_out = self.transformer(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=src_pad_mask,
+                                           tgt_key_padding_mask=tgt_pad_mask)
         out = self.out(transformer_out)
         return out
 
