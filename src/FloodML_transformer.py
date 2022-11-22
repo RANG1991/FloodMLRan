6a329910519f2df4b6d56b737f56d83c1e15b851
Ran <galun.ran@gmail.com>
2022-11-22 13:43:08 +0200

    modifying transformer code to include only the decoder

--- src/FloodML_transformer.py
+++ src/FloodML_transformer.py
@@ -10,24 +10,15 @@ class ERA5_Transformer(nn.Module):
     """
 
     # Constructor
-    def __init__(self, num_tokens, dim_model, num_heads, num_encoder_layers, num_decoder_layers, dropout_p):
+    def __init__(self, num_tokens, dim_model, num_heads, num_encoder_layers, dropout_p):
         super().__init__()
 
-        # INFO
-        self.model_type = "Transformer"
         self.dim_model = dim_model
-
-        # LAYERS
         self.positional_encoder = PositionalEncoding(max_seq_length=num_tokens, encodings_dim=dim_model,
                                                      dropout=dropout_p)
         self.embedding = nn.Embedding(num_tokens, dim_model)
-        self.transformer = nn.Transformer(
-            d_model=dim_model,
-            nhead=num_heads,
-            num_encoder_layers=num_encoder_layers,
-            num_decoder_layers=num_decoder_layers,
-            dropout=dropout_p,
-        )
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=dim_model, nhead=num_heads)
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_encoder_layers)
         self.out = nn.Linear(dim_model, num_tokens)
 
     @staticmethod
@@ -53,21 +44,15 @@ class ERA5_Transformer(nn.Module):
         # [False, False, False, True, True, True]
         return matrix == pad_token
 
-    def forward(self, src, tgt, tgt_mask=None, src_pad_mask=None, tgt_pad_mask=None):
+    def forward(self, src):
         # Embedding + positional encoding - Out size = (batch_size, sequence length, dim_model)
         src = self.embedding(src) * math.sqrt(self.dim_model)
-        tgt = self.embedding(tgt) * math.sqrt(self.dim_model)
         src = self.positional_encoder(src)
-        tgt = self.positional_encoder(tgt)
-
         # we permute to obtain size (sequence length, batch_size, dim_model),
         src = src.permute(1, 0, 2)
-        tgt = tgt.permute(1, 0, 2)
-
         # Transformer blocks - Out size = (sequence length, batch_size, num_tokens)
-        transformer_out = self.transformer(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=src_pad_mask,
-                                           tgt_key_padding_mask=tgt_pad_mask)
-        out = self.out(transformer_out)
+        transformer_encoder_out = self.transformer_encoder(src)
+        out = self.out(transformer_encoder_out)
         return out
 
 
