more fixes to normalization

src/FloodML_LSTM.py
@@ -7,16 +7,15 @@ class LSTM(torch.nn.Module):
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
 
-        self.dropout = dropout
+        self.dropout = torch.nn.Dropout(dropout)
         self.lstm = torch.nn.LSTM(
             input_size=self.input_dim,
             hidden_size=self.hidden_dim,
-            batch_first=True,
-            bias=True,
+            batch_first=True
         )
         self.head = torch.nn.Linear(in_features=self.hidden_dim, out_features=1)
 
     def forward(self, x):
         output, (h_n, c_n) = self.lstm(x)
-        output = torch.nn.Dropout(self.dropout)(output)
+        output = self.dropout(output)
         return self.head(output[:, -1, :])