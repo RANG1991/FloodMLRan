7095ff16078c6880d9b638bfc118b2afbabcda11
Ran <galun.ran@gmail.com>
2023-01-28 14:48:13 +0200

    continue to implement CONV LSTM

--- src/FloodML_2_LSTM.py
+++ src/FloodML_2_LSTM.py
@@ -36,6 +36,7 @@ class TWO_LSTM(torch.nn.Module):
         h_n = self.linear_states(h_n).reshape(batch_size, self.num_channels, self.image_width, self.image_height)
         c_n = self.linear_states(c_n).reshape(batch_size, self.num_channels, self.image_width, self.image_height)
         output = self.conv_lstm(x_spatial, h_n, c_n)
-        output = self.dropout(output)
+        output = output.reshape(batch_size, -1)
         pred = self.head(output)
+        pred = self.dropout(pred)
         return pred
--- src/FloodML_Conv_LSTM.py
+++ src/FloodML_Conv_LSTM.py
@@ -11,28 +11,28 @@ class FloodML_Conv_LSTM(nn.Module):
         self.filter_size_conv = 3
         self.conv_x_input_gate = torch.nn.Conv2d(
             in_channels=in_channels_cnn, out_channels=in_channels_cnn,
-            kernel_size=(self.filter_size_conv, self.filter_size_conv))
+            kernel_size=(self.filter_size_conv, self.filter_size_conv), padding="same")
         self.conv_h_input_gate = torch.nn.Conv2d(
             in_channels=in_channels_cnn, out_channels=in_channels_cnn,
-            kernel_size=(self.filter_size_conv, self.filter_size_conv))
+            kernel_size=(self.filter_size_conv, self.filter_size_conv), padding="same")
         self.conv_x_forget_gate = torch.nn.Conv2d(
             in_channels=in_channels_cnn, out_channels=in_channels_cnn,
-            kernel_size=(self.filter_size_conv, self.filter_size_conv))
+            kernel_size=(self.filter_size_conv, self.filter_size_conv), padding="same")
         self.conv_h_forget_gate = torch.nn.Conv2d(
             in_channels=in_channels_cnn, out_channels=in_channels_cnn,
-            kernel_size=(self.filter_size_conv, self.filter_size_conv))
+            kernel_size=(self.filter_size_conv, self.filter_size_conv), padding="same")
         self.conv_x_output_gate = torch.nn.Conv2d(
             in_channels=in_channels_cnn, out_channels=in_channels_cnn,
-            kernel_size=(self.filter_size_conv, self.filter_size_conv))
+            kernel_size=(self.filter_size_conv, self.filter_size_conv), padding="same")
         self.conv_h_output_gate = torch.nn.Conv2d(
             in_channels=in_channels_cnn, out_channels=in_channels_cnn,
-            kernel_size=(self.filter_size_conv, self.filter_size_conv))
+            kernel_size=(self.filter_size_conv, self.filter_size_conv), padding="same")
         self.conv_x_cell_gate = torch.nn.Conv2d(
             in_channels=in_channels_cnn, out_channels=in_channels_cnn,
-            kernel_size=(self.filter_size_conv, self.filter_size_conv))
+            kernel_size=(self.filter_size_conv, self.filter_size_conv), padding="same")
         self.conv_h_cell_gate = torch.nn.Conv2d(
             in_channels=in_channels_cnn, out_channels=in_channels_cnn,
-            kernel_size=(self.filter_size_conv, self.filter_size_conv))
+            kernel_size=(self.filter_size_conv, self.filter_size_conv), padding="same")
         self.sequence_length = sequence_length
         self.image_width = image_width
         self.image_height = image_height
@@ -41,20 +41,11 @@ class FloodML_Conv_LSTM(nn.Module):
         for i in range(self.sequence_length):
             c_prev = c_curr
             h_prev = h_curr
-            curr_x = x[:, i, :, :, :].squeeze()
+            curr_x = x[:, i, :, :, :]
             input_gate = nn.Sigmoid()(self.conv_x_input_gate(curr_x) + self.conv_h_input_gate(h_prev))
             forget_gate = nn.Sigmoid()(self.conv_x_forget_gate(curr_x) + self.conv_h_forget_gate(h_prev))
             output_gate = nn.Sigmoid()(self.conv_x_output_gate(curr_x) + self.conv_h_output_gate(h_prev))
             c_curr = forget_gate * c_prev + input_gate * nn.Tanh()(self.conv_x_cell_gate(curr_x) +
                                                                    self.conv_h_cell_gate(h_prev))
             h_curr = output_gate * torch.nn.Tanh()(c_curr)
-            max_width_right = int(self.image_width / 2)
-            max_width_left = math.ceil(self.image_width / 2)
-            max_height_right = int(self.image_height / 2)
-            max_height_left = math.ceil(self.image_height / 2)
-            pad = (max_width_left - int(c_curr.shape[1] / 2), max_width_right - math.ceil(c_curr.shape[1] / 2),
-                   max_height_left - int(c_curr.shape[2] / 2), max_height_right - math.ceil(c_curr.shape[2] / 2))
-            pad = (0, 0) + (0, 0) + pad
-            c_curr = F.pad(c_curr, pad, "constant", 0)
-            h_curr = F.pad(h_curr, pad, "constant", 0)
         return h_curr
