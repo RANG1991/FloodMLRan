b5504c52ff7af598bf8c18753a18ddb07c72fe42
Ran <galun.ran@gmail.com>
2023-02-08 16:01:16 +0200

    making the script of parsing slurm file more robust

--- src/parse_slurm_output_file_NSE.py
+++ src/parse_slurm_output_file_NSE.py
@@ -1,35 +1,50 @@
 import re
 import pandas as pd
 from pathlib import Path
+import functools
 
 
-def read_output_file(output_file, basins_ids_to_include=set()):
-    dict_nse_basins = {}
-    with open(output_file, "r") as f:
+def read_output_file(output_file):
+    dicts_models_dict = {}
+    model_name = "empty_model_name"
+    with open(output_file, "r", encoding="utf-8") as f:
         for row in f:
-            find = re.search("station with id: (.*?) has nse of: (.*?)\n", row)
-            if find:
-                basin_id = find.group(1)
-                if len(basins_ids_to_include) > 0 and basin_id not in basins_ids_to_include:
-                    continue
-                basin_nse = float(find.group(2))
-                dict_nse_basins[basin_id] = basin_nse
-    return dict_nse_basins
+            match_model_name_string = re.search("running with model: (.*?)\n", row)
+            if match_model_name_string:
+                new_model_name = match_model_name_string.group(1)
+                if new_model_name != model_name:
+                    print(f"moving to new model with name: {new_model_name}")
+                    dicts_models_dict[new_model_name] = {}
+                    model_name = new_model_name
+            match_nse_string = re.search("station with id: (.*?) has nse of: (.*?)\n", row)
+            if match_nse_string:
+                basin_id = match_nse_string.group(1)
+                basin_nse = float(match_nse_string.group(2))
+                dicts_models_dict[model_name][basin_id] = basin_nse
+    return dicts_models_dict
 
 
-def generate_csv_from_output_file(output_file, basins_ids_to_include=set()):
-    basins_dict = read_output_file(output_file, basins_ids_to_include=basins_ids_to_include)
+def generate_csv_from_output_file(output_file, static_attr_file):
+    basins_dict = read_output_file(output_file)
     output_file_name = Path(output_file).stem
-    pd.DataFrame(basins_dict.values(), columns=['NSE'], index=basins_dict.keys()).to_csv(
-        f"./{output_file_name}.csv")
-    return basins_dict
+    basins_ids = functools.reduce(lambda basin_ids_1, basin_ids_2: basin_ids_1.intersection(basin_ids_2),
+                                  [set(basins_dict[model_name].keys()) for model_name in basins_dict.keys()])
+    basins_dict_for_data_frame = {}
+    for basins_id in basins_ids:
+        list_nse_different_models = [basins_id]
+        for model_name in basins_dict.keys():
+            list_nse_different_models.append(basins_dict[model_name][basins_id])
+        basins_dict_for_data_frame[basins_id] = list_nse_different_models[:]
+    df_nse = pd.DataFrame.from_dict(basins_dict_for_data_frame, orient="index",
+                                    columns=["basin_id"] + [f'NSE_{model_name}' for model_name in basins_dict.keys()])
+    df_static_attrib = pd.read_csv(static_attr_file)
+    df_static_attrib["gauge_id"] = df_static_attrib["gauge_id"].apply(lambda x: x.replace("us_", ""))
+    df_res = df_nse.set_index('basin_id').join(df_static_attrib.set_index('gauge_id'))
+    df_res.to_csv(output_file_name + ".csv")
 
 
 def main():
-    basins_dict_conv_lstm = generate_csv_from_output_file("./output_file_conv_lstm.txt")
-    basins_set_ids_conv_lstm = set(basins_dict_conv_lstm.keys())
-    basins_dict_lstm = generate_csv_from_output_file("./output_file_lstm.txt",
-                                                     basins_ids_to_include=basins_set_ids_conv_lstm)
+    generate_csv_from_output_file("./slurm-5703010.out", "../data/ERA5/Caravan/attributes/attributes_hydroatlas_us.csv")
 
 
 if __name__ == "__main__":
