c1bf655c655f6ba59f13e131ed7f3b3327cde3e1
Ran <galun.ran@gmail.com>
2023-03-01 19:27:12 +0200

    adding ambedding size as variable in LSTM class

--- src/FloodML_LSTM.py
+++ src/FloodML_LSTM.py
@@ -5,17 +5,18 @@ class LSTM(torch.nn.Module):
     def __init__(self, hidden_dim, dropout, num_dynamic_attr, num_static_attr):
         super(LSTM, self).__init__()
         self.input_dim = (num_dynamic_attr + num_static_attr)
+        self.embedding_size = 30
         self.num_dynamic_attr = num_dynamic_attr
         self.num_static_attr = num_static_attr
         self.hidden_dim = hidden_dim
         self.dropout = torch.nn.Dropout(dropout)
         self.lstm = torch.nn.LSTM(
-            input_size=(self.num_dynamic_attr + 10),
+            input_size=(self.num_dynamic_attr + self.embedding_size),
             hidden_size=self.hidden_dim,
             batch_first=True
         )
         self.head = torch.nn.Linear(in_features=self.hidden_dim, out_features=1)
-        self.embedding = torch.nn.Linear(in_features=self.num_static_attr, out_features=10)
+        self.embedding = torch.nn.Linear(in_features=self.num_static_attr, out_features=self.embedding_size)
 
     def forward(self, x):
         x_d = x[:, :, :self.num_dynamic_attr]
