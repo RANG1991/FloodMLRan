794b5a66b90b946ded28c394853be505af18460d
Ran <galun.ran@gmail.com>
2023-03-03 12:50:42 +0200

    changing bash script from running ERA5

--- src/run_LSTM_Ran_implementation.sh
+++ src/run_LSTM_Ran_implementation.sh
@@ -19,7 +19,7 @@ source $virtual_env
 for i in 1 2 3 4 5
 do
   echo "run number: $i"
-  NCCL_P2P_DISABLE=1 python ./FloodML_train_test.py --model LSTM --dataset CARAVAN --optim Adam --num_epochs 15 --sequence_length_spatial 7 --num_workers_data_loader 2 --create_new_files
-#  NCCL_P2P_DISABLE=1 python ./FloodML_train_test.py --model LSTM --dataset CARAVAN --optim Adam --num_epochs 50 --sequence_length_spatial 7 --limit_size_above_1000
+  NCCL_P2P_DISABLE=1 python ./FloodML_train_test.py --model CNN_LSTM --dataset CARAVAN --optim Adam --num_epochs 10 --sequence_length_spatial 7 --limit_size_above_1000 --num_workers_data_loader 2 --create_new_files --use_all_static_attr
+  NCCL_P2P_DISABLE=1 python ./FloodML_train_test.py --model LSTM --dataset CARAVAN --optim Adam --num_epochs 10 --sequence_length_spatial 7 --limit_size_above_1000 --num_workers_data_loader 2 --create_new_files --use_all_static_attr
 #  NCCL_P2P_DISABLE=1 python ./FloodML_train_test.py --model CONV_LSTM --dataset CARAVAN --optim Adam --num_epochs 15 --sequence_length_spatial 7 --limit_size_above_1000
 done
