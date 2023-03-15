abf1cdaa19728204210947313309d0ceac698ffc
Ran <galun.ran@gmail.com>
2023-03-15 15:00:07 +0200

    changing downloading radar data script

--- src/preprocess_precip_radar.py
+++ src/preprocess_precip_radar.py
@@ -1,9 +1,10 @@
-import metpy
-from siphon.catalog import TDSCatalog
-from datetime import datetime, timedelta
+from siphon.ncss import NCSS
+from datetime import datetime
 
-# Set up access via NCSS
-gfs_catalog = "https://cida.usgs.gov/thredds/catalog.xml?dataset=USGS THREDDS Holdings/National Stage IV Quantitative " \
-              "Precipitation Estimate Mosaic"
-cat = TDSCatalog(gfs_catalog)
-dataset = cat.datasets["National Stage IV Quantitative Precipitation Estimate Mosaic"]
+URL = "https://cida.usgs.gov/thredds/ncss/stageiv_combined/"
+ncss = NCSS(URL)
+query = ncss.query()
+query.time_range(datetime(2002, 1, 1, 0, 0, 0), datetime(2002, 1, 1, 0, 0, 1)).strides(spatial=100)
+query.accept("netcdf")
+query.variables('Total_precipitation_surface_1_Hour_Accumulation')
+data = ncss.get_data(query)
