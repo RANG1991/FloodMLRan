67fca4a6dcb2fa38d745fd3d5ea144a49592e8f0
Ran <galun.ran@gmail.com>
2023-04-02 08:34:34 +0300

    create bash file for grib conversion

---
+++ src/run_grib_radar_files_conversion.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -n 2
+#SBATCH --time=100:0:0
+#SBATCH --mem=160G
+
+# Uncomment and enter path of code
+cd sci/labs/efratmorin/ranga/FloodMLRan/data/data.eol.ucar.edu/pub/download/extra/katz_data/stage4/
+
+# virtual_env location
+virtual_env=/sci/labs/efratmorin/ranga/PythonEnvFloodML/bin/activate
+
+source $virtual_env
+
+find . -name '*.Z' -type f -exec wgrib -text {} +
+
+
