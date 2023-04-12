f109db2b5be4e79eebd42cd4021d13936fe0d646
Ran <galun.ran@gmail.com>
2023-04-12 06:50:08 +0300

    changing the bash script of running the Transformer_CNN

--- src/run_Transfromer_Ran_implementation.sh
+++ src/run_Transfromer_Ran_implementation.sh
@@ -3,9 +3,9 @@
 # Change number of tasks, amount of memory and time limit according to your needs
 
 #SBATCH -n 10
-#SBATCH --time=150:0:0
-#SBATCH --mem=160G
-#SBATCH --gres gpu:a30:1
+#SBATCH --time=100:0:0
+#SBATCH --mem=100G
+#SBATCH --gres gpu:a100-3-40
 
 # Uncomment and enter path of code
 cd /sci/labs/efratmorin/ranga/FloodMLRan/src/
@@ -19,5 +19,5 @@ source $virtual_env
 for i in 1 2 3
 do
   echo "run number: $i"
-  NCCL_P2P_DISABLE=1 python ./FloodML_train_test.py --model Transformer_CNN --dataset CARAVAN --optim Adam --num_epochs 15 --sequence_length_spatial 7 --num_workers_data_loader 5
+  NCCL_P2P_DISABLE=1 python ./FloodML_train_test.py --model Transformer_CNN --dataset CARAVAN --optim Adam --num_epochs 10 --sequence_length_spatial 7 --num_workers_data_loader 3 --use_all_static_attr
 done
