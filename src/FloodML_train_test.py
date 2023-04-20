999e55b11988836b6db45292423ed19bbfa28cc2
Ran <galun.ran@gmail.com>
2023-04-20 14:13:42 +0300

    changing basin id in calc_validation_basins_nse function

--- src/FloodML_train_test.py
+++ src/FloodML_train_test.py
@@ -203,7 +203,7 @@ def calc_validation_basins_nse(preds_obs_dict_per_basin, num_epoch, num_basins_f
         nse_list_basins.append(nse)
     # nse_list_basins = torch.cat(nse_list_basins).cpu().numpy()
     nse_list_basins_idx_sorted = np.argsort(np.array(nse_list_basins))
-    median_nse_basin = "01022500"
+    median_nse_basin = "07066000"
     median_nse = statistics.median(nse_list_basins)
     print(f"Basin {median_nse_basin} - NSE: {median_nse:.3f}", flush=True)
     fig, ax = plt.subplots(figsize=(20, 6))
