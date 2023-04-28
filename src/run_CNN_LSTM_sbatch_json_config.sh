11918f1bfd95b7c8cc7ec20fb55a43c0f30a6000
Ran <galun.ran@gmail.com>
2023-04-28 19:34:32 +0300

    fixing bugs

--- src/run_CNN_LSTM_sbatch_json_config.sh
+++ src/run_CNN_LSTM_sbatch_json_config.sh
@@ -16,4 +16,4 @@ virtual_env=/sci/labs/efratmorin/ranga/PythonEnvFloodML/bin/activate
 source $virtual_env
 # module load cuda/11.2
 
-python ./FloodML_runner.py --yml_config_file_name config_files_yml/config_run_CNN_LSTM.yml
+python ./FloodML_runner.py --yaml_config_file_name config_files_yml/config_run_CNN_LSTM.yml
