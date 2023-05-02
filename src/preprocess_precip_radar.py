d7765adfcf3a3b1c6bb2250845caa9ac7f7e527c
Ran <galun.ran@gmail.com>
2023-05-02 09:58:30 +0300

    changing redar processing script

--- src/preprocess_precip_radar.py
+++ src/preprocess_precip_radar.py
@@ -9,7 +9,10 @@ import concurrent.futures
 
 
 def convert_all_grb_files_to_netCDF(grib_file_path, grib2_res_folder_path, nc_res_folder_path):
-    grib_file_path_for_reading_using_xarray = grib2_res_folder_path / (grib_file_path.stem + ".grb2")
+    if not (grib2_res_folder_path / grib_file_path.parent).exists():
+        os.mkdir(grib2_res_folder_path / grib_file_path.parent)
+    grib_file_path_for_reading_using_xarray = grib2_res_folder_path / grib_file_path.parent / (
+                grib_file_path.stem + ".grb2")
     if grib_file_path.suffix != ".grb2":
         if grib_file_path.suffix == ".gz":
             with gzip.open(grib_file_path, 'rb') as f_in:
@@ -23,7 +26,9 @@ def convert_all_grb_files_to_netCDF(grib_file_path, grib2_res_folder_path, nc_re
     else:
         shutil.copy(grib_file_path, grib_file_path_for_reading_using_xarray)
     data = xarray.open_dataset(grib_file_path_for_reading_using_xarray, engine='cfgrib')
-    file_path_net_CDF = nc_res_folder_path / (grib_file_path.stem + ".nc")
+    if not (grib2_res_folder_path / grib_file_path.parent).exists():
+        os.mkdir(nc_res_folder_path / grib_file_path.parent)
+    file_path_net_CDF = nc_res_folder_path / grib_file_path.parent / (grib_file_path.stem + ".nc")
     data.to_netcdf(file_path_net_CDF)
     nc_file = netCDF4.Dataset(file_path_net_CDF)
     print(nc_file.variables)
@@ -42,13 +47,13 @@ def main(use_multiprocessing=True):
     if not grib2_res_folder_path.exists():
         os.mkdir(grib2_res_folder_path)
     else:
-        files = grib2_res_folder_path.glob('*.*')
+        files = grib2_res_folder_path.glob('**/*.*')
         for f in files:
             os.remove(f)
     if not nc_res_folder_path.exists():
         os.mkdir(nc_res_folder_path)
     else:
-        files = nc_res_folder_path.glob('*.*')
+        files = nc_res_folder_path.glob('**/*.*')
         for f in files:
             os.remove(f)
     if use_multiprocessing:
