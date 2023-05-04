b68c97230f2be811430ce3ba8e4bd5bfc7859045
Ran <galun.ran@gmail.com>
2023-05-04 13:04:35 +0300

    fixes + creating new plots

--- src/create_CDF_NSE_comparison_plot_from_slurm_output.py
+++ src/create_CDF_NSE_comparison_plot_from_slurm_output.py
@@ -82,7 +82,7 @@ def create_dict_basin_id_to_NSE_my_code(logs_filename):
 
 
 def plot_NSE_CDF_graphs_my_code():
-    input_file_names = ["slurm-6584894.out", "slurm-6584893.out"]
+    input_file_names = ["slurm-6584893.out", "slurm-6608804.out"]
     input_file_paths = [Path(file_name).resolve() for file_name in input_file_names]
     dict_all_files = {}
     for input_file_path in input_file_paths:
