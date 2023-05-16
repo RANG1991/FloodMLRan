42a8dae92af042aeb6317eb9d5fbece1506b9073
Ran <galun.ran@gmail.com>
2023-05-16 22:36:43 +0300

    fixing Transformer training by changing to batch_first

--- src/FloodML_Transformer_Encoder.py
+++ src/FloodML_Transformer_Encoder.py
@@ -32,19 +32,19 @@ class Transformer_Encoder(nn.Module):
         self.positional_encoding = PositionalEncoding(intermediate_dim, sequence_length)
         encoder_layer = nn.TransformerEncoderLayer(d_model=intermediate_dim, nhead=num_heads, batch_first=True)
         self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
-        self.fc_2 = nn.Linear(intermediate_dim, 16)
+        self.fc_2 = nn.Linear(intermediate_dim, 1)
         exponential_decay = torch.exp(torch.tensor([-1 * (sequence_length - i) / 25 for i in range(sequence_length)]))
         exponential_decay = exponential_decay.unsqueeze(0).unsqueeze(-1).repeat(1, 1, intermediate_dim)
         self.dropout = torch.nn.Dropout(dropout)
         self.intermediate_dim = intermediate_dim
         self.register_buffer('exponential_decay', exponential_decay)
-        self.fc_3 = nn.Linear(16, 1)
+        # self.fc_3 = nn.Linear(16, 1)
 
     def forward(self, x):
         out_fc_1 = self.fc_1(x)
         out_pe = self.positional_encoding(out_fc_1)
         out_transformer = self.transformer_encoder(out_pe)[:, 0, :]
         # out_decay = torch.sum(out_transformer * self.exponential_decay, dim=1)
+        # out_fc_2 = self.fc_2(self.dropout(out_transformer))
         out_fc_2 = self.fc_2(self.dropout(out_transformer))
-        out_fc_3 = self.fc_3(self.dropout(out_fc_2))
-        return out_fc_3
+        return out_fc_2
