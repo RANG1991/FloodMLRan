a317dfa589e88c137545596acb9f7f2f4dd53b26
Ran <galun.ran@gmail.com>
2023-05-23 14:42:37 +0300

    more analysis of results

--- src/analyse_results_NSE_basins.py
+++ src/analyse_results_NSE_basins.py
@@ -1,28 +1,43 @@
 import pandas as pd
-from sklearn.datasets import load_iris
-from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 from sklearn import tree
 from matplotlib import pyplot as plt
+from sklearn.linear_model import LogisticRegression
 
 
-def analyse_results(csv_results_file_with_static_attr):
-    clf = DecisionTreeClassifier(random_state=0, max_depth=3)
+def fit_clf_analysis(csv_results_file_with_static_attr, clf):
     df_results = pd.read_csv(csv_results_file_with_static_attr)
     df_results["label"] = np.where(df_results['NSE_CNN_LSTM_135'] > df_results['NSE_LSTM_135'], 1, 0)
     df_results = df_results.drop(columns=['NSE_CNN_LSTM_135', 'NSE_LSTM_135'])
     df_results = df_results.set_index("basin_id")
     df_results = df_results.select_dtypes(include=[np.number]).dropna()
     clf.fit(df_results.to_numpy()[:, :-1], df_results["label"])
-    fig = plt.figure(figsize=(25, 20))
-    tree.plot_tree(clf, feature_names=df_results.columns[:-1],
-                   class_names=["0", "1"])
-    plt.savefig("decision_tree.png")
+    return clf, df_results
+
+
+def analyse_results_by_decision_tree(csv_results_file_with_static_attr):
+    clf = DecisionTreeClassifier(random_state=0, max_depth=3)
+    clf, df_results = fit_clf_analysis(csv_results_file_with_static_attr, clf)
+    importance = clf.feature_importances_
+    plt.figure(figsize=(25, 20))
+    plt.xticks(rotation=45)
+    plt.bar([x for x in df_results.columns[:-1]], importance)
+    plt.savefig("decision_tree_analysis.png")
+
+
+def analyse_results_by_logistic_regression(csv_results_file_with_static_attr):
+    clf = LogisticRegression()
+    clf, df_results = fit_clf_analysis(csv_results_file_with_static_attr, clf)
+    importance = clf.coef_[0]
+    plt.figure(figsize=(25, 20))
+    plt.xticks(rotation=45)
+    plt.bar([x for x in df_results.columns[:-1]], importance)
+    plt.savefig("logistic_regression_analysis.png")
 
 
 def main():
-    analyse_results("7307546_7479540.csv")
+    analyse_results_by_logistic_regression("7307546_7479540.csv")
 
 
 if __name__ == "__main__":
