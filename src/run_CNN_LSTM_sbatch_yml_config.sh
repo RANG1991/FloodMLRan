e9761ab5e9690f7650b0b187fcfbf42047574098
Ran <galun.ran@gmail.com>
2023-06-05 17:53:06 +0300

    changes to sbatch script - return multiple GPUs

--- src/run_CNN_LSTM_sbatch_yml_config.sh
+++ src/run_CNN_LSTM_sbatch_yml_config.sh
@@ -16,4 +16,4 @@ virtual_env=/sci/labs/efratmorin/ranga/PythonEnvFloodML/bin/activate
 source $virtual_env
 # module load cuda/11.2
 
-python ./FloodML_runner.py --yaml_config_file_name config_files_yml/config_run_CNN_LSTM.yml
+NCCL_P2P_LEVEL=NVL python ./FloodML_runner.py --yaml_config_file_name config_files_yml/config_run_CNN_LSTM.yml
