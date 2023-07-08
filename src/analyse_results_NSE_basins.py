3e67b802b84228ddb868c1ebdd1e7e20f3daaf8f
Ran <galun.ran@gmail.com>
2023-07-08 07:52:45 +0300

    fixes

--- src/analyse_results_NSE_basins.py
+++ src/analyse_results_NSE_basins.py
@@ -176,6 +176,7 @@ def create_class_activation_maps_explainable(checkpoint_path):
         if basin_id != curr_basin_id:
             basin_id_to_first_ind[basin_id] = ind
             curr_basin_id = basin_id
+    list_all_images = []
     for basin_id in basin_id_to_first_ind.keys():
         print(f"in basin: {basin_id}")
         _, _, xs_non_spatial, xs_spatial, _, _ = dataset[basin_id_to_first_ind[basin_id]]
@@ -185,17 +186,29 @@ def create_class_activation_maps_explainable(checkpoint_path):
         plt.tight_layout()
         image_basin = cv2.imread(f"/sci/labs/efratmorin/ranga/FloodMLRan"
                                  f"/data/basin_check_precip_images/img_{basin_id}_precip.png")
+        # image_basin = cv2.resize(image_basin, (256, 256), interpolation=cv2.INTER_CUBIC)
         cmap_image_precip = plt.get_cmap("binary")
         cmap_image_activation = plt.get_cmap("jet")
         # image_basin = cmap_image_precip(image_basin[:, :, :3])
         image_activation = cv2.resize(activation_map[0].cpu().numpy().mean(axis=0), (36, 36),
                                       interpolation=cv2.INTER_CUBIC)
-        image_activation = ((image_activation - image_activation.min())
-                            / (image_activation.max() - image_activation.min()))
+        # image_activation = ((image_activation - image_activation.min())
+        #                     / (image_activation.max() - image_activation.min()))
         image_activation = (255 * (cmap_image_activation(image_activation)[:, :, :3])).astype(np.uint8)
         opacity = 0.7
         overlay = (opacity * image_basin + (1 - opacity) * image_activation)
-        cv2.imwrite(f"./heat_maps/heat_map_basin_{basin_id}.png", np.hstack([image_activation, image_basin]))
+        image_basin_with_margin = cv2.copyMakeBorder(image_basin, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=255)
+        image_activation_with_margin = cv2.copyMakeBorder(image_activation, 10, 10, 10, 10, cv2.BORDER_CONSTANT,
+                                                          value=255)
+        list_all_images.append(np.hstack([image_basin_with_margin, image_activation_with_margin]))
+    list_images_row = []
+    list_rows = []
+    for i in range(len(list_all_images)):
+        if (i + 1) % 4 == 0:
+            list_rows.append(np.hstack(list_images_row))
+            list_images_row = []
+        list_images_row.append(list_all_images[i])
+    cv2.imwrite(f"./heat_maps/heat_map_all_basins.png", np.vstack(list_rows))
 
 
 def main():
