e0c5f1dd36cbf8c499cd86b0674f891cb09b86ba
Ran <galun.ran@gmail.com>
2023-09-28 12:24:44 +0300

    fixing drawing contours

--- src/CAMELS_dataset.py
+++ src/CAMELS_dataset.py
@@ -469,18 +469,14 @@ class Dataset_CAMELS(FloodML_Base_Dataset):
                     contours, _ = cv2.findContours(precipitation_spatial_data_image.copy().astype(np.uint8),
                                                    cv2.RETR_EXTERNAL,
                                                    cv2.CHAIN_APPROX_NONE)
-                    precipitation_spatial_data_image_for_draw = ((precipitation_spatial_data_image -
-                                                                  precipitation_spatial_data_image.min()) /
-                                                                 (precipitation_spatial_data_image.max() -
-                                                                  precipitation_spatial_data_image.min())) * 255
-                    cv2.drawContours(precipitation_spatial_data_image_for_draw, contours, -1, 255, 1)
-                    precipitation_spatial_data_image_for_draw = ax1.imshow(precipitation_spatial_data_image_for_draw)
-                    cb = plt.colorbar(precipitation_spatial_data_image_for_draw, ax=ax1, shrink=0.75)
-                    labels = np.linspace(precipitation_spatial_data_image.min(),
+                    cv2.drawContours(precipitation_spatial_data_image, contours, -1,
+                                     int(precipitation_spatial_data_image.max()), 1)
+                    precipitation_spatial_data_image = ax1.imshow(precipitation_spatial_data_image)
+                    bounds = np.linspace(precipitation_spatial_data_image.min(),
                                          precipitation_spatial_data_image.max(), 10)
-                    loc = labels + .5
-                    cb.set_ticks(loc)
-                    cb.set_ticklabels(labels)
+                    norm = matplotlib.colors.BoundaryNorm(bounds, ncolors=256)
+                    plt.colorbar(precipitation_spatial_data_image, norm=norm, extend='max', ticks=bounds,
+                                 ax=ax1, shrink=0.75)
                     plt.savefig(f"../data/basin_check_precip_images/img_{station_id}"
                                 f"_precip{'_SR' if self.use_super_resolution else '_large' if self.use_large_size else ''}.png")
                     plt.close()
